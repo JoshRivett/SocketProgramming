0d249701d1f87b4caebad582e31a161ac3b54fe5
khanjason <jasonxdecember@gmail.com>
2019-11-02 15:08:31 +0000

    Added basic python server and client. uses sample code from socket library

---
+++ socket.py
@@ -0,0 +1,19 @@
+import socket
+s=socket.socket(type=socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 80))
+s.listen(5)
+while True:
+    # accept connections from outside
+    (clientsocket, address) = s.accept()
+    # now do something with the clientsocket
+    # in this case, we'll pretend this is a threaded server
+    #ct = client_thread(clientsocket)
+    #ct.run()
+    with clientsocket:
+        print('Connected by', address)
+        while True:
+            data = clientsocket.recv(1024)
+            print(data)
+            if not data:
+                break
+            clientsocket.sendall(data)
